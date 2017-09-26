74660f66cdb792554b04620d5326a356cc1824ec
AriellaRogueN <arrodriguezn@gmail.com>
2017-09-26 17:58:03 -0300

    esto es una prueba

---
+++ app.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <title>Un pequeño documento</title>
+</head>
+<body>
+  <h1>Titulo principal de mi documento</h1>
+  <!-- Los titulos usan "h" + "n", siendo "n" un numero del 1 al 6  -->
+  <p>Mira Ma, estoy programando con <abbr title="Hyper Text Markup Language">HTML</abbr>.</p>
+</body>
+</html>
